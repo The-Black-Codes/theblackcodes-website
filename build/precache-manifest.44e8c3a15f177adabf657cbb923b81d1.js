79d01d655b085c916160e34e0182de925ded161c
Tony Fletcher <tfletch55@gmail.com>
2020-04-22 21:11:09 -0400

    Production build

---
+++ build/precache-manifest.44e8c3a15f177adabf657cbb923b81d1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82c29590b25c4dc646c5ab89f28b7933",
+    "url": "/index.html"
+  },
+  {
+    "revision": "515039416e18175d686a",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "4317f678c3603aad11d9",
+    "url": "/static/css/main.94e80b33.chunk.css"
+  },
+  {
+    "revision": "515039416e18175d686a",
+    "url": "/static/js/2.5d62b82a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.5d62b82a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08d976c34e9d5ec9c694",
+    "url": "/static/js/3.22e9ddea.chunk.js"
+  },
+  {
+    "revision": "3f2617840c3cb853a875",
+    "url": "/static/js/4.7453f861.chunk.js"
+  },
+  {
+    "revision": "80442cd36cf71528ff02",
+    "url": "/static/js/5.cc28af28.chunk.js"
+  },
+  {
+    "revision": "020bf407375a5331cfa0",
+    "url": "/static/js/6.a5027e53.chunk.js"
+  },
+  {
+    "revision": "4317f678c3603aad11d9",
+    "url": "/static/js/main.138b510e.chunk.js"
+  },
+  {
+    "revision": "c4e8630902cd1f6c820a",
+    "url": "/static/js/runtime-main.cab06683.js"
+  },
+  {
+    "revision": "e7fb447ae2eab62fe93eb5a877307aa2",
+    "url": "/static/media/BlackCodes.e7fb447a.png"
+  }
+]);
