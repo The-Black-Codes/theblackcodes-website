01bcad4bb629cfff404220cfc411b355a014ad09
Tony Fletcher <tfletch@Tonys-iMac.local>
2020-04-26 23:02:38 -0400

    Contact Form submission

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, { useState } from 'react';
+import { Container, Row, Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import axios from 'axios';
 
-const ContactForm = (props) => {
+const ContactForm = _ => {
+  const [form, setForm] = useState({});
+
+  const submitForm = async () => {
+    await axios.post('http://localhost:8001/contact', form, {
+      headers: {
+        'content-type': 'application/json'
+      }
+    });
+  };
+
   return (
-    <Form>
-      <FormGroup>
-        <Label for="exampleEmail"><h2>Email</h2></Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="Plese enter your email..." />
-      </FormGroup>
-      <FormGroup>
-        <Label for="name"><h2>Name</h2></Label>
-        <Input type="text" name="name" id="name" placeholder="Please enter your Name..." />
-      </FormGroup>
-    
-      <FormGroup>
-        <Label for="exampleText"><h2>Message</h2></Label>
-        <Input type="textarea" name="message" id="exampleText" placeholder="Please leave us a message..." />
-      </FormGroup>
-    
-      <Button><p>Submit</p></Button>
-    </Form>
+    <Container className="contact-page my-5">
+      <Row>
+        <Col xs={12} md={6}>
+        
+        </Col>
+        <Col>
+          <Form>
+            <FormGroup>
+              <Label for="email"><h4>Email</h4></Label>
+              <Input type="email" id="email" onChange={(event) => setForm({ ...form, [event.target.id]: event.target.value })} placeholder="Please enter your email..." />
+            </FormGroup>
+            <FormGroup>
+              <Label for="name"><h4>Name</h4></Label>
+              <Input type="text" id="name" onChange={(event) => setForm({ ...form, [event.target.id]: event.target.value })} placeholder="Please enter your Name..." />
+            </FormGroup>
+          
+            <FormGroup>
+              <Label for="message"><h4>Message</h4></Label>
+              <Input type="textarea" id="message" onChange={(event) => setForm({ ...form, [event.target.id]: event.target.value })} placeholder="Please leave us a message..." />
+            </FormGroup>
+          
+            <Button block color="success" onClick={submitForm}>Submit</Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
