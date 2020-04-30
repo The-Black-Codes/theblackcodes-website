c18445bad83cc95f56838450c01ce6b89f4689ba
Glenn Dixon <glenndixon19@gmail.com>
2020-04-29 20:53:27 -0500

    added original black codes story to our about, deleted aboutImg, center the how we do section

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,10 +10,19 @@ const About = () => {
           lead="Learn all about The Black.Codes."
         />
 
-        <Container>
+        <Container className="themed-container" fluid="true">
           <Row className="about-section">
             <Col md={6} xs={12}>
-              <div class="about-img"></div>
+              <div class="about-og-codes">
+                <h2>Original Black Codes</h2>
+                <p className="og-codes-info">
+                  The Black Codes, sometimes called Black Laws, were laws
+                  governing the conduct of African Americans. The best known of
+                  them were passed in 1865 and 1866 by Southern states, after
+                  the American Civil War, in order to restrict African
+                  Americans' freedom, and to compel them to work for low wages.
+                </p>
+              </div>
             </Col>
             <Col md={6} xs={12}>
               <h2>Get to Know Us</h2>
@@ -26,14 +35,14 @@ const About = () => {
           </Row>
           <Row className="mission-state py-5">
             <Col md={6} xs={12}>
-              <h2>Mission Statment</h2>
+              <h2 className="text-center">Mission Statment</h2>
               <p className="statement">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
             </Col>
           </Row>
-          <Row className="my-5">
+          <Row className="my-5 three-sections">
             <Col md={4} xs={12} className="who-are-we">
               <h2>Who we are?</h2>
               <p>We are a platform for black tech culture!</p>
@@ -53,8 +62,8 @@ const About = () => {
               </p>
             </Col>
           </Row>
-          <Row>
-            <Col md={6} xs={12} className="how-we-do">
+          <Row className="how-we-do">
+            <Col md={6} xs={12}>
               <h2>How do we do it?</h2>
               <p>
                 Through events, collaborations, hackathons, community support,
