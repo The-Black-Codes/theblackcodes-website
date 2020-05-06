47fdba289bbf4f01e481a373b92b39553384ff48
Tony Fletcher <tfletch55@gmail.com>
2020-05-06 15:12:46 -0400

    Production build

---
+++ build/precache-manifest.95ef35332dec1dbe14c11e91056913e5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc95bd8b344be47b6a238f5bd6dfe2b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed216cb3d5e8ef497244",
+    "url": "/static/css/2.f39ea103.chunk.css"
+  },
+  {
+    "revision": "988ba341bc02fc7d0e03",
+    "url": "/static/css/main.370a2c5e.chunk.css"
+  },
+  {
+    "revision": "ed216cb3d5e8ef497244",
+    "url": "/static/js/2.0caae370.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0caae370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "988ba341bc02fc7d0e03",
+    "url": "/static/js/main.ddefe977.chunk.js"
+  },
+  {
+    "revision": "950dbc51068180ed1b45",
+    "url": "/static/js/runtime-main.522aa353.js"
+  },
+  {
+    "revision": "e7fb447ae2eab62fe93eb5a877307aa2",
+    "url": "/static/media/BlackCodes.e7fb447a.png"
+  },
+  {
+    "revision": "61fa5d610defa0bd3bb0bf1f1a3aecaf",
+    "url": "/static/media/portrait.61fa5d61.jpg"
+  },
+  {
+    "revision": "0f6a2da60d4bc8696ecb5e070c84d16c",
+    "url": "/static/media/wide-angle_kindful.0f6a2da6.jpg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fa78329c7599a2656369fce7ca5daf60.js"
+  "/precache-manifest.95ef35332dec1dbe14c11e91056913e5.js"
 );
 
 self.addEventListener('message', (event) => {
