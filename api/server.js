a4b88d60b228decaa22297a5a820c33b28768ae0
Tony Fletcher <tfletch55@gmail.com>
2021-01-10 23:29:36 -0500

    Updated contact form api to use google service account.

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const nodemailer = require('nodemailer');
 
 require('dotenv').config();
+const key = require('./key.json');
 
 server.use(cors());
 server.use(parser.urlencoded({ extended: false }));
@@ -14,34 +15,43 @@ server.get('/', (req, res) => res.send('Working'));
 
 server.post('/contact', async (req, res) => {
   const { name, email, message } = req.body;
-  
-  const client = nodemailer.createTransport({
-    service: 'gmail',
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
-      user: process.env.SG_USER,
-      pass: process.env.G_PASS
+      type: 'OAuth2',
+      user: process.env.SG_EMAIL,
+      serviceClient: key.client_id,
+      privateKey: key.private_key
     }
   });
 
-  client.sendMail({
-    from: process.env.SG_EMAIL,
+  let sendEmail;
+  const options = {
+    from: `The Black Codes Contact Form - ${process.env.SG_EMAIL}`,
     to: process.env.SG_EMAIL,
     subject: 'From The Black.Codes Contact Form',
     html: `<div>
             <h3>Name: ${name} - ${email}</h3></div>
             <h4>Message: <br />${message}</h4>
           </div>`
-  }, async (err, info) => {
-    if (err) {
-      console.log(err);
-    }
-
-    await axios.post("https://hooks.slack.com/services/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", {
-      username: 'The Black.Codes Contact Form',
-      text: `Name: ${name} \n Email: ${email} \n Message: ${message}`
-    })
-    .catch(e => res.json({ err: true, message: e }));
-    return res.json({ err: null, message: 'Done!!' });
+  };
+  
+  try {
+    await transporter.verify();
+    sendEmail = await transporter.sendMail(options, async (err, info) => {
+      if (err) return res.json({ err: true, message: err });
+  
+      await axios.post("https://hooks.slack.com/services/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", {
+        username: 'The Black.Codes Contact Form',
+        text: `Name: ${name} \n Email: ${email} \n Message: ${message}`
+      }).catch(e => res.json({ err: true, message: e }));
+    });
+  } catch (err) {
+    console.error(err);
+    return res.json({ err: true, message: err });
   });
 });
 
